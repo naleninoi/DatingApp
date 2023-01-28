09dc52dee17ffcf04e69deb80c0dbdcd0a976ee0
Viktor Kochetov-Arhipov <naleninoi@gmail.com>
2023-01-28 16:31:08 +0300

    3-24 add HttpClient

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Dating App';
+  users: any;
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:7001/api/users').subscribe(
+      response => {
+        this.users = response;
+      },
+      error => {
+        console.error(error);
+      });
+  }
+
 }
