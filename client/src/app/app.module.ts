7e66034935953bce62e55141e62b10913b60acd9
Viktor Kochetov-Arhipov <naleninoi@gmail.com>
2023-02-06 15:52:36 +0300

    5-60 add register form

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,12 +9,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
