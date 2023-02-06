c5d8548f6664f9c33a7898a1564c100f6c4a88cd
Viktor Kochetov-Arhipov <naleninoi@gmail.com>
2023-02-06 15:33:19 +0300

    5-59 add home page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,11 +8,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
