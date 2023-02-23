4666b69f924fe7eb35c52fe16dd766030f91e331
Viktor Kochetov-Arhipov <naleninoi@gmail.com>
2023-02-23 18:21:52 +0300

    7-080 add errors interceptor

---
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { NavigationExtras, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor(
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError(error => {
+        if (error) {
+          switch (error.status) {
+            case 400:
+              const errorsObject = error.error.errors;
+              if (errorsObject) {
+                const modalStateErrors = [];
+                for (const key in errorsObject) {
+                  if (errorsObject[key]) {
+                    modalStateErrors.push(errorsObject[key]);
+                  }
+                }
+                throw modalStateErrors;
+              } else {
+                this.toastr.error(error.error, error.status);
+              }
+              break;
+            case 401:
+              this.toastr.error('Not authorized', error.status);
+              break;
+            case 404:
+              this.router.navigate(['/not-found']);
+              break;
+            case 500:
+              const navigationExtras: NavigationExtras = {
+                state: {error: error.error}
+              }; 
+              this.router.navigate(['/server-error'], navigationExtras);
+              break;
+            default:
+              this.toastr.error('Something unxpected went wrong...');
+              console.error(error);
+              break;
+          }
+        }
+        return throwError(error);
+      })
+    );
+  }
+}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -15,6 +15,7 @@ import { ListsComponent } from './components/lists/lists.component';
 import { MessagesComponent } from './components/messages/messages.component';
 import { SharedModule } from './_modules/shared.module';
 import { TestErrorsComponent } from './components/errors/test-errors/test-errors.component';
+import { ErrorInterceptor } from './_interceptors/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -36,7 +37,9 @@ import { TestErrorsComponent } from './components/errors/test-errors/test-errors
     FormsModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
