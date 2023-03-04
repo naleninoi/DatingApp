42710613e443e48e44a89cccc089c98fab853204
Viktor Kochetov-Arhipov <naleninoi@gmail.com>
2023-03-04 14:13:13 +0300

    8-094 add UserRepository

---
+++ API/Data/UserRepository.cs
@@ -0,0 +1,44 @@
+using API.Entities;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        async Task<AppUser> IUserRepository.GetUserByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        async Task<AppUser> IUserRepository.GetUserByUsernameAsync(string username)
+        {
+            return await _context.Users
+                .Include(u => u.Photos)
+                .SingleOrDefaultAsync(u => u.UserName == username);
+        }
+
+        async Task<IEnumerable<AppUser>> IUserRepository.GetUsersAsync()
+        {
+            return await _context.Users
+                .Include(u => u.Photos)
+                .ToListAsync();
+        }
+
+        async Task<bool> IUserRepository.SaveAllAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        void IUserRepository.Update(AppUser user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+        }
+    }
+}
--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -10,6 +10,8 @@ namespace API.Extensions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
             services.AddScoped<ITokenService, TokenService>();
+
+            services.AddScoped<IUserRepository, UserRepository>();
             
             services.AddDbContext<DataContext>(options =>
             {
