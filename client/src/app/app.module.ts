e5a846043e9a7b7599ed520a59d4edbb9a4bb36e
Viktor Kochetov-Arhipov <naleninoi@gmail.com>
2023-03-08 17:27:19 +0300

    9-106 add member card

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { TestErrorsComponent } from './components/errors/test-errors/test-errors
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { NotFoundComponent } from './components/errors/not-found/not-found.component';
 import { ServerErrorComponent } from './components/errors/server-error/server-error.component';
+import { MemberCardComponent } from './components/members/member-card/member-card.component';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { ServerErrorComponent } from './components/errors/server-error/server-er
     TestErrorsComponent,
     NotFoundComponent,
     ServerErrorComponent,
+    MemberCardComponent,
   ],
   imports: [
     BrowserModule,
