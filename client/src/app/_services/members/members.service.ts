ff5a60e72240fa8054ee0e2abeaa36af11b35e39
Viktor Kochetov-Arhipov <naleninoi@gmail.com>
2023-03-11 13:12:57 +0300

    9-109 aqd JWT interceptor

--- client/src/app/_services/members/members.service.ts
+++ client/src/app/_services/members/members.service.ts
@@ -1,6 +1,5 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { API_URLS } from 'src/app/_infrastructure/api-urls';
 import { Member } from 'src/app/_models/member';
 import { environment } from 'src/environments/environment';
@@ -13,20 +12,14 @@ export class MembersService {
   baseUrl = environment.apiUrl;
   membersEndpoint = API_URLS.members.list;
 
-  readonly httpOptions = {
-    headers: new HttpHeaders({
-      Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user')).token
-    })
-  };
-
   constructor(private http: HttpClient) {
   }
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + this.membersEndpoint, this.httpOptions);
+    return this.http.get<Member[]>(this.baseUrl + this.membersEndpoint);
   }
 
   getMember(username: string) {
-    return this.http.get<Member>(this.baseUrl + this.membersEndpoint + username, this.httpOptions);
+    return this.http.get<Member>(this.baseUrl + this.membersEndpoint + username);
   }
 }
