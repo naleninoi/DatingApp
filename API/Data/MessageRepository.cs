d554600dabaaeb0e8ed6a613e0609747f701cfbd
Viktor Kochetov-Arhipov <naleninoi@gmail.com>
2023-08-15 20:44:24 +0300

    15-187 get messages

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -6,16 +6,20 @@ using API.DTO;
 using API.Entities;
 using API.Helpers;
 using API.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 
 namespace API.Data
 {
     public class MessageRepository : IMessageRepository
     {
         private readonly DataContext _context;
+        private readonly IMapper _mapper;
 
-        public MessageRepository(DataContext context)
+        public MessageRepository(DataContext context, IMapper mapper)
         {
-            this._context = context;
+            _mapper = mapper;
+            _context = context;
         }
 
         public void AddMessage(Message message)
@@ -33,9 +37,21 @@ namespace API.Data
             return await _context.Messages.FindAsync(id);
         }
 
-        public Task<PagedList<MessageDto>> GetMessagesForUser()
+        public async Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams)
         {
-            throw new NotImplementedException();
+            var query = _context.Messages
+                .OrderByDescending(m => m.MessageSent)
+                .AsQueryable();
+            
+            query = messageParams.Container switch
+            {
+                "Inbox" => query.Where(m => m.Recipient.UserName == messageParams.Username),
+                "Outbox" => query.Where(m => m.Sender.UserName == messageParams.Username),
+                _ => query.Where(m => m.Recipient.UserName == messageParams.Username && m.DateRead == null)
+            };
+
+            var messages = query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider);
+            return await PagedList<MessageDto>.CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
         }
 
         public Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId, int recipientId)
