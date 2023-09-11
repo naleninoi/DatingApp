97756dc510fa65d918d42a7f72511d5caf51ab22
Viktor Kochetov-Arhipov <naleninoi@gmail.com>
2023-09-11 17:40:54 +0300

    using query params

--- client/src/app/components/members/member-detail/member-detail.component.ts
+++ client/src/app/components/members/member-detail/member-detail.component.ts
@@ -34,6 +34,11 @@ export class MemberDetailComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadMember();
+
+    this.route.queryParams.subscribe(params => {
+      params.tab ? this.selectTab(params.tab) : this.selectTab(0);
+    });
+
     this.galleryOptions = [{
       width: '500px',
       height: '500px',
@@ -78,6 +83,10 @@ export class MemberDetailComponent implements OnInit {
     if (this.activeTab.heading === 'Messages' && this.messages.length === 0) {
       this.loadMessages();
     }
+  }
+
+  selectTab(tabId: number) {
+    this.memberTabs.tabs[tabId].active = true;
   }
 
 }
